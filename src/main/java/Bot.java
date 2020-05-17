d22c5e81d8b3eab74989c9502185cbe52b97b4f8
L2 <jvisual00@gmail.com>
2020-05-17 21:17:50 +0400

    5/17/20

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,10 +1,39 @@
+import com.sun.deploy.security.ruleset.Rule;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.nio.channels.Channel;
 
-public class Bot {
+/*
+*
+*
+*
+*  BOT ID IS 710743646697422978
+*
+*
+*
+*/
+111
+public class Bot extends ListenerAdapter {
     public static void main(String[] args) throws Exception {
-        JDA jda = new JDABuilder("NzEwNzQzNjQ2Njk3NDIyOTc4.Xr8hCA.DZ82AkPsqFsnnMp9JYEgh7itKh8").build();
+        JDA jda = new JDABuilder("NzEwNzQzNjQ2Njk3NDIyOTc4.Xr8hCA.DZ82AkPsqFsnnMp9JYEgh7itKh8").addEventListeners(new Bot()).setActivity(Activity.playing("Type &help")).build();
+        jda.addEventListener();
+
+        //make by reading file
+    }
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){
+        //make if log file is empty (write in docs command &start)
+        //&start (set member rule,create new text channel and write this id and write there in file)
+        //628227546374340630
+        MessageChannel ch=event.getChannel();
+        TextChannel tc= event.getTextChannel();
+        System.out.println(ch);
+        String message = event.getMessage().getContentRaw().toLowerCase();
+        if (message.equals("&start"));
     }
 }
---
+++ src/main/java/First_Init.java
@@ -0,0 +1,9 @@
+import com.sun.deploy.security.ruleset.Rule;
+
+public class First_Init {
+    private Rule memberRule;
+
+    protected First_Init(Rule memberRule){
+
+    }
+}
