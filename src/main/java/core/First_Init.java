3885970a256de37849553b0c9291be7cd679858d
Lmp-off <Jelesha@protonmail.ch>
2020-05-22 07:52:16 +0400

    add Languages
    add simple language system

--- src/main/java/core/First_Init.java
+++ src/main/java/core/First_Init.java
@@ -6,7 +6,6 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class First_Init {
-    //es
     private static User InitUser;
     private static boolean isInitialize=false;
     private static String memberRoleID;
