2b0fb29441970182a94ca8ecd2c7282b65c838f3
Lmp-off <Jelesha@protonmail.ch>
2020-06-11 13:48:03 +0400

    fix
    12

--- src/main/java/mute/MuteVars.java
+++ src/main/java/mute/MuteVars.java
@@ -2,7 +2,7 @@ package mute;
 
 import java.util.ArrayList;
 
-public class MuteVars {
+public class MuteVars {//serialized in one class with first init vars
     static MuteThread current;
     static ArrayList<Muted> muteds=new ArrayList<>();
 }
