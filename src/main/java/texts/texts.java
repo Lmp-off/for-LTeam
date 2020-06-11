b979508b9d0e2f6a60bac710ae7229788b1303db
Lmp-off <Jelesha@protonmail.ch>
2020-06-11 13:52:34 +0400

    12
    12

--- src/main/java/texts/texts.java
+++ src/main/java/texts/texts.java
@@ -5,16 +5,18 @@ public class texts {
             "started my initialization,\nnow you need to enter your private text channel ID where you can control me(&f-to show all my functions)"
             ,"Initialization can be continued by "
             ,"\nif you want to cancel initialization tap &stop"
-            ,"Now the last you need to write member role"
+            ,"And the last you need to write member role"
             ,"This is not channel ID\n(right click to your private text channel and click to copy chanel ID"
             ,"init was correct\n••••••••••••••••••\n"
             ," - is the main administrator\n"
-            ," - is the default rule\n"
-            ," - if the root text channel\n"
+            ," - is the default role\n"
+            ," - is the root text channel\n"
             ,"incorrect role id\n"
             ,"My initialization has done by success before if you want to reset it write &stop\n"
             ,": is stopped my initialization >:(\n"
             ,"if u want to stop me first of all u need to start initialization by &start >3\n"
+            ," - is the ban role"
+            ,"Now you need to to send ban role id\n"
 
     };
     //TODO:СЛАВА ЗАПОЛНИ МАССИВ(SLAVA MAKE firstInitRus !empty)
@@ -30,7 +32,7 @@ public class texts {
     };
     public final String[] mat = {"бля","блядь","блядина","бляха","блять","бляд","блят","бл","хуй","хуйцо","хуйня"
             ,"херня","хуесос","сперма","охуел","спэрма","ебало","ебло","хуйнючка","ебалай","мать","ебал","ебать"
-            ,"блэд","рукоблуд","залупа","пенис","хер","давалка","пизда","пиздец","пиздабол","пиздаболл","пипиндра","педик","пидор","fuck"};
+            ,"блэд","рукоблуд","залупа","пенис","хер","давалка","пизда","пиздец","пиздабол","пиздаболл","пипиндра","педик","пидор","fuck","чмо","еба"};
    public final String allCommandsEng="&rus - переключает язык на русский\n&eng - switch your language to English\n&start - write this command to initialize Bot\n";
 
    public final String allCommandsRus="&rus - переключает язык на русский\n&eng - switch your language to English\n&start - эта команда запускает бота\n";
